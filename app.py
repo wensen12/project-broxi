123586167daef0364c481edaa0398bf521aad085
wensen12 <150154094+wensen12@users.noreply.github.com>
2024-11-23 11:42:13 +0700

    app.py

--- app.py
+++ app.py
@@ -3,45 +3,47 @@ import mysql.connector
 
 app = Flask(__name__)
 
-db = mysql.connector.connect(
-    host="wenn",
-    user="root",
-    password="1234",
-    database="mydatabase"
-)
-cursor = db.cursor()
+# Koneksi ke database
+def get_db_connection():
+    conn = mysql.connector.connect(
+        host='localhost',
+        user='wensen',
+        password='12345',
+        database='money_management'
+    )
+    return conn
 
 @app.route('/')
 def index():
-    cursor.execute("SELECT * FROM items")
-    items = cursor.fetchall()
-    return render_template('index.html', items=items)
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM transactions')
+    transactions = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', transactions=transactions)
 
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-        name = request.form['name']
-        cursor.execute("INSERT INTO items (name) VALUES (%s)", (name,))
-        db.commit()
-        return redirect('/')
-    return render_template('add.html')
-
-@app.route('/edit/<int:id>', methods=['GET', 'POST'])
-def edit(id):
-    if request.method == 'POST':
-        name = request.form['name']
-        cursor.execute("UPDATE items SET name = %s WHERE id = %s", (name, id))
-        db.commit()
-        return redirect('/')
-    
-    cursor.execute("SELECT * FROM items WHERE id = %s", (id,))
-    item = cursor.fetchone()
-    return render_template('edit.html', item=item)
+@app.route('/add', methods=['POST'])
+def add_transaction():
+    description = request.form['description']
+    amount = request.form['amount']
+    date = request.form['date']
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO transactions (description, amount, date) VALUES (%s, %s, %s)', (description, amount, date))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return redirect('/')
 
 @app.route('/delete/<int:id>')
-def delete(id):
-    cursor.execute("DELETE FROM items WHERE id = %s", (id,))
-    db.commit()
+def delete_transaction(id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM transactions WHERE id = %s', (id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
     return redirect('/')
 
 if __name__ == '__main__':
